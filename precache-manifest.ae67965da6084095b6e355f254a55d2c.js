3caa86c5887b8afcce9e6fb611b471406ff29167
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2020-09-15 05:25:22 +0700

    Updates

---
+++ precache-manifest.ae67965da6084095b6e355f254a55d2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d3c3d5a112cfabde81fc133d68fbac",
+    "url": "/WibuWeb/index.html"
+  },
+  {
+    "revision": "a82b2c9627f10025cf67",
+    "url": "/WibuWeb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "841e81a50bb23e834801",
+    "url": "/WibuWeb/static/js/2.8f901020.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WibuWeb/static/js/2.8f901020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82b2c9627f10025cf67",
+    "url": "/WibuWeb/static/js/main.aa02e458.chunk.js"
+  },
+  {
+    "revision": "df75de7d2913c6cc5f6b",
+    "url": "/WibuWeb/static/js/runtime-main.19cde79e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WibuWeb/static/media/logo.5d5d9eef.svg"
+  }
+]);
